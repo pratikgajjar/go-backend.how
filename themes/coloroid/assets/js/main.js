4e5c407da5e06cf4f9bd56105905674330feb773
Pratik <pmgajjar25@gmail.com>
2024-08-20 01:05:28 +0530

    Move js

--- themes/coloroid/assets/js/main.js
+++ themes/coloroid/assets/js/main.js
@@ -1,1 +1,15 @@
-console.log("Hello, friend :)");
+document.addEventListener("DOMContentLoaded", function() {
+    const images = document.querySelectorAll('.lazyload-image');
+
+    images.forEach(function(img) {
+        img.addEventListener('load', function() {
+            img.style.opacity = '1';
+            const placeholder = img.previousElementSibling;
+            if (placeholder) {
+                placeholder.style.opacity = '0';
+                placeholder.style.transform = 'scale(1.1)';
+            }
+        });
+    });
+});
+
