5df7e5b606d838bdd7076670fee8895100abb515
Pratik <pmgajjar25@gmail.com>
2024-08-20 01:16:27 +0530

    Handle case when original img loads before blur

--- themes/coloroid/assets/js/main.js
+++ themes/coloroid/assets/js/main.js
@@ -2,14 +2,25 @@ document.addEventListener("DOMContentLoaded", function() {
     const images = document.querySelectorAll('.lazyload-image');
 
     images.forEach(function(img) {
-        img.addEventListener('load', function() {
+        // Check if the image has already loaded
+        if (img.complete) {
             img.style.opacity = '1';
             const placeholder = img.previousElementSibling;
             if (placeholder) {
                 placeholder.style.opacity = '0';
                 placeholder.style.transform = 'scale(1.1)';
             }
-        });
+        } else {
+            // Add an event listener to handle the load event
+            img.addEventListener('load', function() {
+                img.style.opacity = '1';
+                const placeholder = img.previousElementSibling;
+                if (placeholder) {
+                    placeholder.style.opacity = '0';
+                    placeholder.style.transform = 'scale(1.1)';
+                }
+            });
+        }
     });
 });
 
